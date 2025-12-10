eee55fe7e9f061b9f4372071ce79dc2c82354c88
Muhamadamin <125745772+winner1377@users.noreply.github.com>
2025-12-10 14:01:36 +0330

    testing path

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.view import http_test,json_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path ( 'url adress' , 'view' )
+    path('http-test',http_test),
+    path('json-test',json_test)
 ]
