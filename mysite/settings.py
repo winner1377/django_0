3ef900d7c6105a5c26faa23a074e0a62de5738ce
Muhamadamin <125745772+winner1377@users.noreply.github.com>
2025-12-22 08:56:42 +0330

    statics and tags
    i used the tags for the first time2

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'STATIC'
+STATIC_URL = '/statics/'
+STATIC_ROOT = BASE_DIR / 'STATICs'
 
 
 MEDIA_URL = '/media/'
