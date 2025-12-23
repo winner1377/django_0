40d776147394404740cfa1b52827aeb1be77d1fd
Muhamadamin <125745772+winner1377@users.noreply.github.com>
2025-12-23 11:47:17 +0330

    fixed urls
    fixed all the urls

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
     path('', index_view, name='home'),
     path('about',about_view, name= 'about'),
     path('contact',contact_view, name='contact'),
